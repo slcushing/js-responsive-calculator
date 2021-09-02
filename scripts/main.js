f277312723a15e69f24e40d4d604e5672e0dcc6a
Stephanie Cushing <stephaniecushing@Stephanies-MacBook-Air.local>
2021-09-02 15:41:21 -0400

    modifies .gitignore

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+
+const $numbers = document.querySelectorAll(".number");
+console.log($numbers);
+
+// $numbers.addEventListener = ('click', function(){
+//     console.log('You clicked the number.');
+// });
+
+
+ function pushNumber() {
+    let message = ($numbers);
+    alert(message);
+ };
+ pushNumber();
