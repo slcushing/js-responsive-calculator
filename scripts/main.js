42b6982ec8bd56f3ab614b66dad6efb99d56b392
Stephanie Cushing <slcushing@gmail.com>
2021-09-27 19:50:34 -0400

    Total Appears on Calc Display

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,12 @@ const $buttons = document.querySelectorAll("button");
 const $numbers = document.querySelectorAll(".number");
 const $operators = document.querySelectorAll(".operator");
 const $clear = document.querySelectorAll(".clear");
-const $equal = document.querySelectorAll(".equal-sign");
+const $equalSign = document.querySelectorAll(".equal-sign");
+const $display = document.querySelector(".calculator-screen");
+
+
 
 const calculation = [];
-// console.log(calculation.length);
-// console.dir($numbers);
 
 $numbers.forEach(number => {
     number.addEventListener("click", function pushNumber(event){
@@ -20,20 +21,15 @@ $operators.forEach(operator => {
     operator.addEventListener("click", function pushOperator(event){
         console.log(event.target.value);
         calculation.push(event.target.value);
+        
     });
 });
 
-// $clear.forEach(clear => {
-//     clear.addEventListener("click", function clear(event){
-//         alert(event.target.value);
-//     });
-// })
-
-$equal.forEach(equal => {
+$equalSign.forEach (equal => {
     equal.addEventListener("click", function calculate(event){
         // alert(event.target.value);
         let num1 = '', num2 = '', operator = null, result;
-        const operators = ['*', '/', '+', '-']
+        const operators = ['*', '/', '+', '-','=']
         for(let i = 0; i < calculation.length; i++) {
             const char = calculation[i];
             if(operators.includes(char)){
@@ -43,7 +39,7 @@ $equal.forEach(equal => {
             } else {
                 num2 += char;
             }
-        }
+        };
     
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
@@ -57,37 +53,18 @@ $equal.forEach(equal => {
     } else if (operator === '/') {
         result = num1 / num2;
     };
-    
+   const totalResult = result;
+
+   document.querySelector(".calculator-screen").value = totalResult;
     });
 });
 
-
     
-    // switch (operator) [
-    //     case '+':
-    //         return (num1 + num2);
-    //         break;
-    //     case '-':
-    //         return (num1 - num2);
-    //         break;
-    //     case '*' 
-    //         return (num1 * num2);
-    //         break;
-    //     case '/':
-    //         return (num1 / num2);
-    //         break;
-    //     default:
-    //         return = 'Error';
-    // ]
-    // };
-    
-
-
-
-
-
-//currently can get to something like ['90+10='] - have to figure out how to split up the string between numbers and operators then use switch to cycle through the mathematial functions
-
+// $clear.forEach(clear => {
+//     clear.addEventListener("click", function clear(event){
+//         alert(event.target.value);
+//     });
+// })
 
 //if button click is a number, then change from string to number and join if necessary
 //if button click is operator, then register it as its mathematical function
